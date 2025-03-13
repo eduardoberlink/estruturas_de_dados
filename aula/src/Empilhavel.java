f9be9c9be527aaf436ccb61b39d790db767675c8
Eduardo Berlink <eduardoberlink@gmail.com>
2025-03-13 17:40:05 -0300

    Estrutura de dados primeira aula

---
+++ aula/src/Empilhavel.java
@@ -0,0 +1,43 @@
+    public interface Empilhavel {
+    /*
+    Operações principais
+    1-Empilhar//push
+    2-Desempilhar//pop
+    3-Topo//top
+    4-update
+    Operações Auxiliares
+    5-estaCheia//isFull //overFlow
+    6-estaVazia//isEmpty //underFlow
+    7-imprimir//print
+    */
+
+    void push(Object o);
+    Object pop();
+    Object top();
+    boolean isFull();
+    boolean isEmpty();
+    //duas formas:
+    void print1();
+    String print2();
+    void update(Object o);
+  
+} 
+
+ /*
+     C - create
+     R - read
+     U - update
+     D - delete
+     S - sort
+     */
+    
+    
+    //chamada do metodo
+    //pilha.pop();
+    /*
+     Tipos de Dados:
+     number,object...
+     integer,float,ponto fixo,double,long.
+     String,char.
+     boolean.
+     */
