70d5ef7c54e581b5556408b71aec184f389ef10e
Eduardo Berlink <eduardoberlink@gmail.com>
2025-07-17 15:37:17 -0300

    nova arvore avp

---
+++ aula/src/arvore/avp/src/Cor.java
@@ -0,0 +1,14 @@
+package arvore.avp.src;
+
+/**
+ * Enumeração para as cores dos nós da árvore vermelho e preto.
+ * Utilizada para distinguir propriedades e regras de balanceamento.
+ *
+ * @author Eduardo Miranda Berlink Santos
+ * @version 1.0
+ * @since July 1, 2025
+ */
+public enum Cor {
+    VERMELHO,
+    PRETO
+}
